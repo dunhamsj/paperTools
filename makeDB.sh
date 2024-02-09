d66b1b505c903d9d4127862e678e81a68bf0864c
Sam Dunham <9784969+dunhamsj@users.noreply.github.com>
2024-02-09 17:54:58 -0500

    updates

--- makeDB.sh
+++ makeDB.sh
@@ -9,3 +9,4 @@ bibtool -r ${PTOOLSDIR}/bibtoolrsc \
         -f "%-3.1n(author)%4d(year)" \
         -o mainDB.bib
 sed -i 's/{1979/{r1979/g' mainDB.bib # hack to fix van Riper entry
+sed -i 's/{a-d-tab2021/{amrex2021/g' mainDB.bib # hack to fix amrex entry
