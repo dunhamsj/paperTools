e20e4bdd503486da525645c3d16a0bd4a50eda0f
Sam Dunham <9784969+dunhamsj@users.noreply.github.com>
2025-02-21 23:04:23 -0500

    updates

--- makeDB.sh
+++ makeDB.sh
@@ -4,7 +4,11 @@
 # and comment out duplicates
 #bibtool -s -d bib1.bib bib2.bib -o newbib.bib
 
+#bibtool -r ${PTOOLSDIR}/bibtoolrsc \
+#        -i ${PTOOLSDIR}/main.bib \
+#        -f "%-1n(author)%4d(year)" \
+#        -o mainDB.bib
 bibtool -r ${PTOOLSDIR}/bibtoolrsc \
         -i ${PTOOLSDIR}/main.bib \
-        -f "%-1n(author)%4d(year)" \
+        -f "%-3.1n(author)%4d(year)" \
         -o mainDB.bib
